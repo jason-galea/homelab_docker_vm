246f375acfb8bd4d5b4bc4f469791d5364e008f7
Jason Galea <nunya@business.meme>
2025-05-13 01:12:50 +1000

    feat: start refresh

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/bash
 
-echo refresh
+# ### TODO
+# echo -e "\n==> Refreshing services"
+# REPOS=(
+#     # homelab_nginx
+#     # homelab_pihole
+#     price_scraper
+#     # bookstack
+#     # unifi
+#     # wekan
+# )
+# for REPO in "${REPOS[@]}"; do
 
-git pull
+#     echo -e "\n==> Cloning $REPO"
+#     if [ ! -d $REPO ]; then
+#         git clone https://github.com/jason-galea/$REPO.git
+#     fi
+
+#     (
+#         cd $REPO
+#         pwd
+#         echo -e "\n==> Starting $REPO"
+#         docker compose up -d
+#     )
+#     sleep 1
+# done
